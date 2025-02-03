15d67ee2582fd378be29c9ba9b72682cec8ef48c
Carlos Eduardo <144180629+eaicaduu@users.noreply.github.com>
2025-02-03 18:02:37 -0300

    Adicionando tema dark

---
+++ js/screen.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function showSplashScreen() {
+        const splashScreen = document.createElement('div');
+        splashScreen.id = 'splash-screen';
+        splashScreen.style.position = 'fixed';
+        splashScreen.style.top = '0';
+        splashScreen.style.left = '0';
+        splashScreen.style.width = '100%';
+        splashScreen.style.height = '100%';
+        splashScreen.style.backgroundColor = '#007bff';
+        splashScreen.style.display = 'flex';
+        splashScreen.style.justifyContent = 'center';
+        splashScreen.style.alignItems = 'center';
+        splashScreen.style.zIndex = '9999';
+        splashScreen.style.transition = 'opacity 1s ease-out'; 
+
+    
+        const logo = document.createElement('img');
+        logo.src = 'images/logo_global.png'; 
+        logo.width = 300;
+        splashScreen.appendChild(logo);
+    
+        document.body.appendChild(splashScreen);
+
+        setTimeout(function () {
+            splashScreen.style.opacity = '0'; 
+            splashScreen.style.overflow = 'hidden';
+            setTimeout(function () {
+                splashScreen.style.display = 'none';
+                document.getElementById('main-content').style.display = 'none'; 
+            }, 2000);
+        }, 2000); 
+    }
+
+    showSplashScreen();
+});
