0bbdb0883de55110637c2d40fef9d559a0dcdf68
Carlos Eduardo <144180629+eaicaduu@users.noreply.github.com>
2025-04-02 17:59:17 -0300

    Plano oculto phone

--- modules/planos.php
+++ modules/planos.php
@@ -1,65 +1,253 @@
-<aside>
-    <div class="py-4">
+<aside class="d-flex">
+    <div class="py-4 me-auto position-relative"
+        style="z-index:1; width:40%;box-shadow: 40px 0px 20px rgb(255, 255, 255);"></div>
+    <div class="py-4 ms-auto" style="z-index:0;width:60%;">
+        <div id="scroll-container" class="d-flex justify-content-evenly  ms-4"
+            style="cursor: grab; white-space: nowrap; overflow-x: hidden; scroll-snap-type: x mandatory; -webkit-overflow-scrolling: touch;">
+            <div class="container d-flex justify-content-center align-items-center my-4">
+                <div class="card border-0 rounded-4 p-3 text-center"
+                    style="width: 350px; background: linear-gradient(135deg, #007bff, #001f4f); color: white;">
 
-        <div class="d-flex justify-content-evenly" style="overflow-x: auto; white-space: nowrap;">
-
-            <div class="container d-flex justify-content-center align-items-center my-5">
-                <div class="card shadow-lg border-0 rounded-4 p-4 text-center"
-                    style="max-width: 400px; background: linear-gradient(135deg, #007bff, #003f7f); color: white;">
-
-                    <div class="d-flex flex-column align-items-center">
-                        <img src="images/wifi_dark_24dp.png" class="pe-none mb-3" width="60" height="60">
-                        <h1 class="fw-bold display-4">900</h1>
-                        <span class="fs-5 text-uppercase">MEGAS</span>
-                        <h6 class="fw-light mt-2">Plano Ultra</h6>
+                    <div class="d-flex justify-content-between align-items-start mb-2">
+                        <div class="d-flex align-items-center">
+                            <h1 class="fw-bold display-6">900</h1>
+                            <span class="fs-6 ms-1 text-uppercase mt-1">MEGAS</span>
+                        </div>
+                        <img src="images/wifi.png" class="pe-none ms-2" width="40" height="40">
                     </div>
 
-                    <hr class="border-light my-4">
+                    <h6 class="fw-light mb-2">Plano Premium <img src="images/verified.png" class="ms-1" width="16"
+                            height="16"></h6>
+                    <hr class="border-light my-2">
 
                     <div>
-                        <h4>
+                        <h4 class="mb-2">
                             <span class="fs-6">Oferta por</span> <br>
-                            <span class="fs-5">R$</span>
-                            <span class="display-5">179,90</span>
+                            <span class="fs-6">R$</span>
+                            <span class="display-6 fw-bold">179,90</span>
                             <span class="fs-6">/mês</span>
-                            <br>
-                            <span class="fs-6">Até o vencimento</span>
                         </h4>
+                        <span class="fs-6 d-block">Até o vencimento</span>
                     </div>
 
                     <a href="https://api.whatsapp.com/send?phone=5551991437617"
-                        class="btn btn-warning w-100 rounded-pill fw-bold mt-4 py-2 d-flex align-items-center justify-content-center shadow-lg"
-                        style="color: white; text-decoration: none; font-size: 1.2rem;">
-                        <img src="images/whatsapp.png" class="me-2" width="24" height="24"> Contratar
+                        class="btn btn-warning w-100 rounded-pill fw-bold mt-3 py-2 d-flex align-items-center justify-content-center shadow-lg"
+                        style="color: white; text-decoration: none; font-size: 1rem;">
+                        <img src="images/whatsapp.png" class="me-2" width="20" height="20"> CONTRATAR
                     </a>
 
-                    <div class="border-top border-light mt-4 pt-3">
-                        <div class="d-flex mb-2 justify-content-center">
-                            <img src="images/play.png" width="20px" height="20px" class="pe-none me-1">
-                            <h6>Streaming</h6>
+                    <hr class="border-light my-2">
+
+                    <div class="mt-2">
+                        <div class="d-flex justify-content-center align-items-center mb-2">
+                            <img src="images/play.png" width="16px" height="16px" class="pe-none me-1">
+                            <h6 class="m-0 p-0">Streaming Incluso</h6>
                         </div>
-                        <div class="d-flex mb-2">
-                            <img src="images/logo_max.png" style="border-radius: 10px;" width="100"
-                                class="pe-none bg-primary py-4 px-3">
-                            <img src="images/logo_telecine.png" style="border-radius: 10px;" width="120"
-                                class="pe-none bg-primary py-4 px-2 ms-2">
+                        <div class="d-flex flex-wrap justify-content-center gap-2 pe-none">
+                            <img src="images/1.png" class="rounded-3" width="70">
+                            <img src="images/2.png" class="rounded-3" width="70">
+                            <img src="images/3.png" class="rounded-3" width="70">
+                            <img src="images/4.png" class="rounded-3" width="70">
                         </div>
-                        <div class="d-flex">
-                            <img src="images/logo_combate.png" style="border-radius: 10px;" width="120"
-                                class="pe-none bg-primary py-4 px-3">
-                            <img src="images/logo_panamout.png" style="border-radius: 10px;" width="100"
-                                class="pe-none bg-primary ms-2">
-                        </div>
-                        <div class="d-flex justify-content-center">
-                            <img src="images/streaming.png" class="pe-none" width="200" class="mt-2">
+                        <div class="d-flex justify-content-center mt-2">
+                            <img src="images/streaming.png" class="pe-none" width="180">
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="container d-flex justify-content-center align-items-center my-4">
+                <div class="card border-0 rounded-4 p-3 text-center"
+                    style="width: 350px; background: linear-gradient(135deg, #007bff, #001f4f); color: white;">
 
+                    <div class="d-flex justify-content-between align-items-start mb-2">
+                        <div class="d-flex align-items-center">
+                            <h1 class="fw-bold display-6">600</h1>
+                            <span class="fs-6 ms-1 text-uppercase mt-1">MEGAS</span>
+                        </div>
+                        <img src="images/wifi.png" class="pe-none ms-2" width="40" height="40">
+                    </div>
 
+                    <h6 class="fw-light mb-2">Plano Ultra <img src="images/verified.png" class="ms-1" width="16"
+                            height="16"></h6>
+                    <hr class="border-light my-2">
+
+                    <div>
+                        <h4 class="mb-2">
+                            <span class="fs-6">Oferta por</span> <br>
+                            <span class="fs-6">R$</span>
+                            <span class="display-6 fw-bold">129,90</span>
+                            <span class="fs-6">/mês</span>
+                        </h4>
+                        <span class="fs-6 d-block">Até o vencimento</span>
+                    </div>
+
+                    <a href="https://api.whatsapp.com/send?phone=5551991437617"
+                        class="btn btn-warning w-100 rounded-pill fw-bold mt-3 py-2 d-flex align-items-center justify-content-center shadow-lg"
+                        style="color: white; text-decoration: none; font-size: 1rem;">
+                        <img src="images/whatsapp.png" class="me-2" width="20" height="20"> CONTRATAR
+                    </a>
+
+                    <hr class="border-light my-2">
+
+                    <div class="mt-2">
+                        <div class="d-flex justify-content-center align-items-center mb-2">
+                            <img src="images/play.png" width="16px" height="16px" class="pe-none me-1">
+                            <h6 class="m-0 p-0">Streaming Incluso</h6>
+                        </div>
+                        <div class="d-flex flex-wrap justify-content-center gap-2 pe-none">
+                            <img src="images/1.png" class="rounded-3" width="70">
+                            <img src="images/3.png" class="rounded-3" width="70">
+                        </div>
+                        <div class="d-flex justify-content-center mt-2">
+                            <img src="images/streaming.png" class="pe-none" width="180">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container d-flex justify-content-center align-items-center my-4">
+                <div class="card border-0 rounded-4 p-3 text-center"
+                    style="width: 350px; background: linear-gradient(135deg, #007bff, #001f4f); color: white;">
+
+                    <div class="d-flex justify-content-between align-items-start mb-2">
+                        <div class="d-flex align-items-center">
+                            <h1 class="fw-bold display-6">400</h1>
+                            <span class="fs-6 ms-1 text-uppercase mt-1">MEGAS</span>
+                        </div>
+                        <img src="images/wifi.png" class="pe-none ms-2" width="40" height="40">
+                    </div>
+
+                    <h6 class="fw-light mb-2">Plano Smart <img src="images/verified.png" class="ms-1" width="16"
+                            height="16"></h6>
+                    <hr class="border-light my-2">
+
+                    <div>
+                        <h4 class="mb-2">
+                            <span class="fs-6">Oferta por</span> <br>
+                            <span class="fs-6">R$</span>
+                            <span class="display-6 fw-bold">99,90</span>
+                            <span class="fs-6">/mês</span>
+                        </h4>
+                        <span class="fs-6 d-block">Até o vencimento</span>
+                    </div>
+
+                    <a href="https://api.whatsapp.com/send?phone=5551991437617"
+                        class="btn btn-warning w-100 rounded-pill fw-bold mt-3 py-2 d-flex align-items-center justify-content-center shadow-lg"
+                        style="color: white; text-decoration: none; font-size: 1rem;">
+                        <img src="images/whatsapp.png" class="me-2" width="20" height="20"> CONTRATAR
+                    </a>
+
+                    <hr class="border-light my-2">
+
+                    <div class="mt-2">
+                        <div class="d-flex justify-content-center align-items-center mb-2">
+                            <img src="images/play.png" width="16px" height="16px" class="pe-none me-1">
+                            <h6 class="m-0 p-0">Streaming Incluso</h6>
+                        </div>
+                        <div class="d-flex flex-wrap justify-content-center gap-2 pe-none">
+                            <img src="images/1.png" class="rounded-3" width="70">
+                        </div>
+                        <div class="d-flex justify-content-center mt-2">
+                            <img src="images/streaming.png" class="pe-none" width="180">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container d-flex justify-content-center align-items-center my-4">
+                <div class="card border-0 rounded-4 p-3 text-center"
+                    style="width: 350px; background: linear-gradient(135deg, #007bff, #001f4f); color: white;">
+
+                    <div class="d-flex justify-content-between align-items-start mb-2">
+                        <div class="d-flex align-items-center">
+                            <h1 class="fw-bold display-6">100</h1>
+                            <span class="fs-6 ms-1 text-uppercase mt-1">MEGAS</span>
+                        </div>
+                        <img src="images/wifi.png" class="pe-none ms-2" width="40" height="40">
+                    </div>
+
+                    <h6 class="fw-light mb-2">Plano Básico <img src="images/verified.png" class="ms-1" width="16"
+                            height="16"></h6>
+                    <hr class="border-light my-2">
+
+                    <div>
+                        <h4 class="mb-2">
+                            <span class="fs-6">Oferta por</span> <br>
+                            <span class="fs-6">R$</span>
+                            <span class="display-6 fw-bold">89,90</span>
+                            <span class="fs-6">/mês</span>
+                        </h4>
+                        <span class="fs-6 d-block">Até o vencimento</span>
+                    </div>
+
+                    <a href="https://api.whatsapp.com/send?phone=5551991437617"
+                        class="btn btn-warning w-100 rounded-pill fw-bold mt-3 py-2 d-flex align-items-center justify-content-center shadow-lg"
+                        style="color: white; text-decoration: none; font-size: 1rem;">
+                        <img src="images/whatsapp.png" class="me-2" width="20" height="20"> CONTRATAR
+                    </a>
+
+                    <hr class="border-light my-2">
+
+                    <div class="mt-2 invisible">
+                        <div class="d-flex justify-content-center align-items-center mb-2">
+                            <img src="images/play.png" width="16px" height="16px" class="pe-none me-1">
+                            <h6 class="m-0 p-0">Streaming Incluso</h6>
+                        </div>
+                        <div class="d-flex flex-wrap justify-content-center gap-2 pe-none">
+                            <img src="images/1.png" class="rounded-3" width="70">
+                        </div>
+                        <div class="d-flex justify-content-center mt-2">
+                            <img src="images/streaming.png" class="pe-none" width="180">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container d-flex justify-content-center align-items-center my-4">
+                <div class="card border-0 rounded-4 p-3 text-center"
+                    style="width: 350px; background: linear-gradient(135deg, #007bff, #001f4f); color: white;">
+
+                    <div class="d-flex justify-content-between align-items-start mb-2">
+                        <div class="d-flex align-items-center">
+                            <h1 class="fw-bold display-6">Telefone</h1>
+                            <span class="fs-6 ms-1 text-uppercase mt-1">Fixo</span>
+                        </div>
+                        <img src="images/call.png" class="pe-none ms-2" width="40" height="40">
+                    </div>
+
+                    <h6 class="fw-light mb-2">Chamadas Ilimitadas <img src="images/verified.png" class="ms-1" width="16"
+                            height="16"></h6>
+                    <hr class="border-light my-2">
+
+                    <div>
+                        <h4 class="mb-2">
+                            <span class="fs-6">A partir de</span> <br>
+                            <span class="fs-6">R$</span>
+                            <span class="display-6 fw-bold">19,90</span>
+                            <span class="fs-6">/mês</span>
+                        </h4>
+                        <span class="fs-6 d-block invisible">CPF ou CNPJ</span>
+                    </div>
+
+                    <a href="https://api.whatsapp.com/send?phone=5551991437617"
+                        class="btn btn-warning w-100 rounded-pill fw-bold mt-3 py-2 d-flex align-items-center justify-content-center shadow-lg"
+                        style="color: white; text-decoration: none; font-size: 1rem;">
+                        <img src="images/whatsapp.png" class="me-2" width="20" height="20"> CONTRATAR
+                    </a>
+
+                    <hr class="border-light my-2">
+
+                    <div class="mt-2 invisible">
+                        <div class="d-flex justify-content-center align-items-center mb-2">
+                            <img src="images/play.png" width="16px" height="16px" class="pe-none me-1">
+                            <h6 class="m-0 p-0">Streaming Incluso</h6>
+                        </div>
+                        <div class="d-flex flex-wrap justify-content-center gap-2 pe-none">
+                            <img src="images/1.png" class="rounded-3" width="70">
+                        </div>
+                        <div class="d-flex justify-content-center mt-2">
+                            <img src="images/streaming.png" class="pe-none" width="180">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
     </div>
-
 </aside>
